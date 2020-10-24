666b997c6554503cbce711d82d8523e3fab6522c
Ramiro Lima <ramirolima.dev@gmail.com>
2020-10-24 17:31:19 -0300

    adjusts

--- db.js
+++ db.js
@@ -1,6 +1,14 @@
-var db = [
-    { "id": 1, "title": "json" },
-    { "id": 2, "title": "json2" }
-  ];
-  
-  module.exports = db;
+//{ "name": "resource1", "hash": "xxxh", "host": "120.0.0.1" },
+let resources = [
+    { "name": "resource1", "hash": "xxxh", "host": "127.0.0.1:3030" },
+    { "name": "resource2", "hash": "xxxh", "host": "127.0.0.1:3031" },
+];
+
+//{ "id": "ip:porta", "lastRequest": "time" },
+let users = [
+    { "id": "127.0.0.1:3030", "lastRequest": "time" },
+    { "id": "127.0.0.1:3031", "lastRequest": "time" },
+
+];
+
+module.exports = { resources, users };
